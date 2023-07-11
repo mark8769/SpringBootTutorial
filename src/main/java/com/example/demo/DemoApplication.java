bcf5edb5ceec79cee6438465ae805c55ad823e48
Mark Ortega <mortegaponce27@gmail.com>
2023-07-10 22:36:31 -0500

    src/main/java/com/examples/demo/DemoApplication.java: Added @GetMapping annotation to new helloWorld function.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,4 +13,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello(){
+		return "Hello World";
+	}
 }
