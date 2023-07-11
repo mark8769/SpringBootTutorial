fd6e34c5e2551085005c8041eda6d28685d771b4
Mark Ortega <mortegaponce27@gmail.com>
2023-07-11 16:44:29 -0500

    Moved controller method code to service layer, calling service layer to get data.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,4 +1,14 @@
 package com.example.demo.student;
 
+import com.sun.tools.javac.util.List;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(1L,"Mariam", "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),21 ));
+    }
 }
