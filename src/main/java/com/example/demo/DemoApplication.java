9459ce9aed865bd9d3a12a147a0e29d508caad80
Mark Ortega <mortegaponce27@gmail.com>
2023-07-10 22:44:52 -0500

    src/main/java/com/examples/demo/DemoApplication.java: Added example json response function, and comments.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,15 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController // Makes this class to serve REST endpoints.
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello(){
-		return "Hello World";
+//	@GetMapping // The only REST point right now.
+//	public String hello(){
+//		return "Hello World";
+//	}
+	@GetMapping // The only REST point right now. Showing how to send a json response.
+	public List<String> jsonResponse(){
+		return List.of("Hello", "World");
 	}
 }
